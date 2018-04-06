27de95670542411649c4ade52132ba751b352c11
Emil Wåreus <emil.wareus47@gmail.com>
2018-04-06 22:42:17 +0800

    debugging

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -73,7 +73,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	// This line creates a normal (Gaussian) distribution for x, y and theta
 	normal_distribution<double> dist_x(x, std[0]);
 	normal_distribution<double> dist_y(y, std[1]);
-	normal_distribution<double> dist_theta(theta, std_[2]);
+	normal_distribution<double> dist_theta(theta, std[2]);
 	
 	for (int i = 0; i < num_particles; ++i) {
 		double sample_x, sample_y, sample_theta;
