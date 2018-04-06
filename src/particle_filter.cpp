78514373e9110dd87ab3dccab1ace5fb269a4091
Emil Wåreus <emil.wareus47@gmail.com>
2018-04-06 22:49:26 +0800

    debugging

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -108,6 +108,7 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 	//  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
 	//  http://www.cplusplus.com/reference/random/default_random_engine/
 
+	cout << "Prediction" << endl;
 	
 	default_random_engine gen;
 	normal_distribution<double> dist_x(0, std_pos[0]);
@@ -129,6 +130,7 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 		particles[i].theta +=  dist_theta(gen);
 	}
 
+	cout << "Done with predict" << endl;
 	
 }
 
