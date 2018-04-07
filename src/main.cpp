4fbabbdf61d85e6b240038f1f234b82219bce0ac
Emil Wåreus <emil.wareus47@gmail.com>
2018-04-07 10:09:16 +0800

    debugging

--- src/main.cpp
+++ src/main.cpp
@@ -109,22 +109,22 @@ int main()
 				obs.y = y_sense[i];
 				noisy_observations.push_back(obs);
         	}
-      cout << "test 0"<< endl;
+     
 		  // Update the weights and resample
 		  pf.updateWeights(sensor_range, sigma_landmark, noisy_observations, map);
 		  pf.resample();
       
 		  // Calculate and output the average weighted error of the particle filter over all time steps so far.
-      cout << "test 1 "<< endl;
+      
 		  vector<Particle> particles = pf.particles;
-      cout << "test 2 "<< endl;
+     
 		  int num_particles = particles.size();
       
 		  double highest_weight = -1.0;
       Particle best_particle;
       double weight_sum = 0.0;
       for (int i = 0; i < num_particles; ++i) {
-        cout << particles[i].weight << endl;
+       
 			if (particles[i].weight > highest_weight) {
 				highest_weight = particles[i].weight;
 				best_particle = particles[i];
